a9d211b9947f235c00a5b98f461f407d69ca6621
alekay2200 <69811800+alekay2200@users.noreply.github.com>
2022-07-09 16:51:26 +0200

    Database config os enviroment

--- nosql_abstract_database.py
+++ nosql_abstract_database.py
@@ -6,6 +6,7 @@ Created on Fri Sept 4 09:39 2020
 from abc import ABC, abstractclassmethod
 from typing import Dict, List, Union, Dict, Set, Generic, TypeVar, Any
 from json import load
+from os import environ
 
 T = TypeVar("T")
 
@@ -17,17 +18,25 @@ class Abstract_Database(ABC, Generic[T]):
     _password: str
     _db_name: str
 
-    def __init__(self, config_file: str):
+    def __init__(self, config_file: Union[str, None]):
         """
+        If path to config file not specify, the system will be read the values from the os variable system.
         - config_file (str): Path to the configuration file necessary to connect to the database server
         """
-        with open(config_file) as f:
-            json_file = load(f)
-            self._ip = json_file["ip"]
-            self._port = json_file["port"]
-            self._username = json_file["username"]
-            self._password = json_file["password"]
-            self._db_name = json_file["db_name"]
+        if config_file is not None:
+            with open(config_file) as f:
+                json_file = load(f)
+                self._ip = json_file["ip"]
+                self._port = json_file["port"]
+                self._username = json_file["username"]
+                self._password = json_file["password"]
+                self._db_name = json_file["db_name"]
+        else:
+            self._ip = environ["ip"]
+            self._port = environ["port"]
+            self._username = environ["username"]
+            self._password = environ["password"]
+            self._db_name = environ["db_name"]
 
     def get_db_name(self) -> str:
         return self._db_name
